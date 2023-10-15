186c11b059a7572d06357cfe2859ccf4bdc4109c
Max Wendel <max@heavyconnect.com>
2023-10-15 02:07:25 -0300

    Fix cors tentativa 2

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create();
-instance.defaults.baseURL='https://pluspet-api.ashystone-e3d6e0e7.eastus.azurecontainerapps.io/';
+instance.defaults.baseURL='https://pluspet-backend-production.up.railway.app/';
 instance.defaults.headers.common['Accept']='*/*';
 instance.defaults.headers.post['Content-Type'] = 'application/json';
 // instance.defaults.headers.common['Authorization'] = ""
